d510aebefa95dbdd5944c26ed3ad85ab51178288
utkarshsanju13 <utkarshsanju13@gmail.com>
2023-02-20 19:45:19 +0530

    first commit

---
+++ src/main/java/com/azure/azuresqlspringboot/AzuresqlSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.azure.azuresqlspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzuresqlSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzuresqlSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azure/azuresqlspringboot/AzuresqlSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.azuresqlspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzuresqlSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
